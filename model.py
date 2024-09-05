92fbc773a453f074d366714d833476183ce59173
Patrick Moorhead <patrickmoorhead@Patricks-MacBook-Pro-2.local>
2024-09-05 13:32:19 -0500

    Added intructions for optimizing

--- model.py
+++ model.py
@@ -1,1 +1,14 @@
+import scikitgit  as sk
+import pandas as pd
 # create model
+
+
+
+
+# Feature Engineering
+
+# Hyperparameter Tuning using GridSearchCV
+
+# Prune the tree using the best hyperparameters
+
+# Crossvalidation using K-fold for tree 
